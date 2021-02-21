9a800a615d298c7e838075d5d81bbf8a0ca524f4
Rudolph Kalp <rfkalp@live.co.za>
2021-02-21 22:38:26 +0200

    Added new version of blue mile certification app.

---
+++ BlueMile.Certification.Data/Models/ImageModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlueMile.Certification.Data.Models
+{
+    public class ImageModel
+    {
+        public Guid Id { get; set; }
+
+        public string ImageName { get; set; }
+
+        public string UniqueImageName { get; set; }
+    }
+}
---
+++ BlueMile.Certification.Data/Models/OwnerModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace BlueMile.Certification.Data.Models
+{
+    public class OwnerModel
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        public string Email { get; set; }
+
+        public string ContactNumber { get; set; }
+
+        public string Identification { get; set; }
+
+        public string AddressLine1 { get; set; }
+
+        public string AddressLine2 { get; set; }
+
+        public string AddressLine3 { get; set; }
+
+        public string AddressLine4 { get; set; }
+
+        public string Suburb { get; set; }
+        
+        public string Town { get; set; }
+
+        public string Province { get; set; }
+
+        public string Country { get; set; }
+
+        public string PostalCode { get; set; }
+    }
+}
