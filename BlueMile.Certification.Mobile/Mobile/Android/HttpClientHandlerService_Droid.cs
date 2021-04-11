44cee67f2b6d3dab16201569d003b3f7bfe98cba
Rudolph Kalp <rfkalp@live.co.za>
2021-04-11 14:10:40 +0200

    Fixed nuget issues

--- BlueMile.Certification.Mobile/Mobile/Android/HttpClientHandlerService_Droid.cs
+++ BlueMile.Certification.Mobile/Mobile/Android/HttpClientHandlerService_Droid.cs
@@ -1,14 +1,7 @@
-﻿using Android.App;
-using Android.Content;
-using Android.OS;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
-using BlueMile.Certification.Mobile.Droid;
+﻿using BlueMile.Certification.Mobile.Droid;
+using BlueMile.Certification.Mobile.Services.InternalServices;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Net.Http;
 using Xamarin.Forms;
 
 
